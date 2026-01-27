20db76efe44842c655804526f07cea0bc2f190cf
zaxrmdn <zramadan000@gmail.com>
2026-01-27 14:38:12 +0700

    update

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const logger = require('./middleware/logger')
 app.use(logger)
 const connection = require('./middleware/db_connect');
 
+app.disable("x-powered-by");
+
 // Dashboard
 app.use('/', express.static(path.join(__dirname, 'public')));
 
