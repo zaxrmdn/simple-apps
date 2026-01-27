4d5c5923319c336734a019b4f850adb3296be338
zaxrmdn <zramadan000@gmail.com>
2026-01-27 14:56:36 +0700

    update

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.get('/app2', (req, res) => {
 app.get('/users', (req, res, next) => {
   const sql = "SELECT * FROM tb_data ORDER BY id desc"
   connection.query(sql,(error, fields) => 
-  res.send(fields)
+  res.send(fields))
 });
 
 app.listen(process.env.APP_PORT, () => {
