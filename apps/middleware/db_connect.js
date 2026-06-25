b61e24d78453723128665f078608653a7a6468a4
zakaria <zakaria@idn.id>
2026-06-25 09:51:24 +0700

    update

--- apps/middleware/db_connect.js
+++ apps/middleware/db_connect.js
@@ -1,8 +1,5 @@
 const mysql = require('mysql')
-<<<<<<< HEAD
 require('dotenv').config();
-=======
->>>>>>> 8f5532c467a68da3df5ed27ddaadd13eeccd4bd4
 
 // Connect database
 const connection = mysql.createConnection({
